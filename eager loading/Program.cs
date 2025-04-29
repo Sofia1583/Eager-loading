4933e579e7663690ae6e1484e38e483c1f9dd758
Unknown <unknown@gmail.com>
2025-04-29 15:14:18 +0500

    добавлены разные способы получения

--- eager loading/Program.cs
+++ eager loading/Program.cs
@@ -28,6 +28,39 @@ using (ApplicationContext db = new ApplicationContext())
 
     db.SaveChanges();
 }
+/*//с использованием метода Include
+using (ApplicationContext db = new ApplicationContext())
+{
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+
+    Company microsoft = new Company { Name = "Microsoft" };
+    Company google = new Company { Name = "Google" };
+    db.Companies.AddRange(microsoft, google);
+
+    User tom = new User { Name = "Tom", Company = microsoft };
+    User bob = new User { Name = "Bob", Company = google };
+    User alice = new User { Name = "Alice", Company = microsoft };
+    User kate = new User { Name = "Kate", Company = google };
+    db.Users.AddRange(tom, bob, alice, kate);
+    db.SaveChanges();
+
+    var users = db.Users.ToList();  // метод Include не используется
+    foreach (var user in users)
+        Console.WriteLine($"{user.Name} - {user.Company?.Name}");
+}
+// с использованием метода db.Companies
+using (ApplicationContext db = new ApplicationContext())
+{
+    var companies = db.Companies.ToList();
+    // получаем пользователей
+    var users = db.Users
+        //.Include(u => u.Company)  // подгружаем данные по компаниям
+        .ToList();
+    foreach (var user in users)
+        Console.WriteLine($"{user.Name} - {user.Company?.Name}");
+}*/
+// с использованием метода ThenInclude()
 using (ApplicationContext db = new ApplicationContext())
 {
     // получаем пользователей
